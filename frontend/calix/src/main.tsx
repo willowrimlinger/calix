1cab6d72a0246dee980dce0d2708be3358b7a3f6
Willow Rimlinger <git@willowr.dev>
2026-02-16 12:23:15 -0500

    Add public domain favicon

--- frontend/calix/src/main.tsx
+++ frontend/calix/src/main.tsx
@@ -5,7 +5,6 @@ import {RouterProvider, createRouter} from "@tanstack/react-router";
 // Import the generated route tree
 import {routeTree} from "./routeTree.gen";
 
-import "./styles.css";
 import reportWebVitals from "./reportWebVitals.ts";
 
 // Create a new router instance
--- frontend/calix/src/routes/__root.tsx
+++ frontend/calix/src/routes/__root.tsx
@@ -1,9 +1,15 @@
 import {Outlet, createRootRoute} from "@tanstack/react-router";
+import "@mantine/core/styles.css";
+import {MantineProvider, createTheme} from "@mantine/core";
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
 
 export const Route = createRootRoute({
     component: () => (
-        <>
+        <MantineProvider theme={theme}>
             <Outlet />
-        </>
+        </MantineProvider>
     ),
 });
